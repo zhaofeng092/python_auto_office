c2a8c7a2d6eae85fbcf4a417401c29ee8b0e8120
lzf <1957875073@qq.com>
2021-01-26 17:22:06 +0800

    update

--- B站/【高级应用】Python自动化办公/code/3.批量重命名.py
+++ B站/【高级应用】Python自动化办公/code/3.批量重命名.py
@@ -17,7 +17,7 @@ for filename in fileList:
 
     if del_replace_content in filename:
         old_name = filename
-        new_name = old_name.replace(del_replace_content, '第')L
+        new_name = old_name.replace(del_replace_content, '')
         os.rename(path + os.sep + old_name, path + os.sep + new_name)
 
         print(filename)
