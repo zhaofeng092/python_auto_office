a1c3653009e54793b2683b7fe7049be03fc75e5e
zhaofeng092 <61504442+zhaofeng092@users.noreply.github.com>
2021-03-07 10:58:33 +0800

    Update 3.批量重命名.py

--- B站/【高级应用】Python自动化办公/code/3.批量重命名.py
+++ B站/【高级应用】Python自动化办公/code/3.批量重命名.py
@@ -13,11 +13,20 @@ del_replace_content = input('请输入需要去掉的内容：')
 # 获取该目录下所有文件，存入列表中
 fileList = os.listdir(path)
 
-for filename in fileList:
+# for filename in fileList:
+#
+#     if del_replace_content in filename:
+#         old_name = filename
+#         new_name = old_name.replace(del_replace_content, '(公众号：Python图书馆，还有更多资料).doc')
+#         os.rename(path + os.sep + old_name, path + os.sep + new_name)
+#
+#         print(filename)
 
-    if del_replace_content in filename:
-        old_name = filename
-        new_name = old_name.replace(del_replace_content, '(公众号：Python图书馆，还有更多资料).doc')
-        os.rename(path + os.sep + old_name, path + os.sep + new_name)
-
-        print(filename)
+def get_files():
+    for filepath,dirnames,filenames in os.walk(path):
+        for filename in filenames:
+            print(os.path.join(filepath,filename))
+            old_name = filename
+            new_name = old_name.replace(del_replace_content, '(公众号：Python图书馆，还有更多资料).pdf')
+            os.rename(os.path.join(filepath,old_name),os.path.join(filepath,new_name))
+get_files()
