d3536a2aa1a5a178b498b1ec15a951fa85a5253d
zhaofeng092 <61504442+zhaofeng092@users.noreply.github.com>
2021-11-06 17:42:25 +0800

    更新 - Python资源合集

--- 团队公众号/Python图书馆/code/19期短视频 - 问题1 - 语文/code.py
+++ 团队公众号/Python图书馆/code/19期短视频 - 问题1 - 语文/code.py
@@ -2,7 +2,7 @@
 # @公众号 :Web图书馆 代码详解：http://t.cn/A6Ihdrxf
 # @Software: PyCharm 安装教程：https://mp.weixin.qq.com/s/a0zoCo9DacvdpIoz1LEN3Q
 # @Description:
-# Python全套学习资源：https://mp.weixin.qq.com/s/s8SM69ioH_UJw_0Ytx8qvg
+# Python全套学习资源：https://mp.weixin.qq.com/s/G_5cY05Qoc_yCXGQs4vIeg
 
 import openpyxl
 
